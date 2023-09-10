ea65777c264a10271b09e6d41db498a8503ef259
Pranav <pranavmuralidharan2004@gmail.com>
2023-09-10 10:19:56 +0530

    Django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -147,3 +147,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+SMTP_PORT = 587
+EMAIL_HOST_USER = 'pranavmuralidharan2004@gmail.com'
+EMAIL_HOST_PASSWORD = 'geeglaguqkmuszks'
+EMAIL_USE_TLS = True
